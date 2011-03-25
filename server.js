9110e85b69af202c0767331ec46dfd6924d80d02
Brad Davis <brad@braddavis.cc>
2011-03-25 01:08:41 -0400

    log in and cookies added

--- server.js
+++ server.js
@@ -1,32 +1,82 @@
 var connect = require('connect'),
     app = require('express').createServer(),
     express = require('express'),
-    db = require('mongous').Mongous,
-    problog = require('./problog.js');
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server,
+    BSON = require('mongodb').BSONNative,
+    posts = require('mongous').Mongous;
 
 app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'superkey'}));
+app.use(express.static(__dirname + '/resources'));
+var db = new Db('local', new Server('127.0.0.1', 27017, {}));
 
 app.get('/', function(req, res){
-  db('local.local').find(5, function(reply){
-    res.render('index.jade', {posts: reply.documents});
-    console.log(reply.documents)
+  db.open(function(err, db){
+    db.collection('local', function(err, collection){
+      collection.find({}, {'sort':[['date', -1]]}, function(err, cursor){
+        cursor.toArray(function(err, posted){
+          if(req.cookies.loggedin == 1){
+            var logged = 'you are logged in';
+          } else {
+            var logged = 'you should log in';
+          }
+          
+          res.render('index.jade', {posts: posted, logged: logged});
+          db.close();
+        });
+      });
+    });
   });
 });
+
 app.get('/new', function(req,res){
+  console.log(req);
   res.render('newpost.jade');  
 });
 app.post('/brandnew', function(req, res){
-  console.log(req.body)
-  db('local.local').save(req.body);
+  console.log(req.body);
+  function postPrep(array){
+    array.date = new Date();
+    array.user = req.cookies.user;
+  };
+  var prepedPost = postPrep(req.body);
+  console.log(prepedPost);
+  posts('local.local').save(prepedPost);
   res.send('saved new post');
 });
+
 app.get('/login', function(req,res){
   res.render('loginform.jade');
 });
 app.post('/login', function(req,res){
-  
-  res.send('You logged in!');  
+  db.open(function(err, db){
+    db.collection('users', function(err, collection){
+      collection.find({}, function(err, cursor){
+        cursor.toArray(function(err, posted){
+          for (var i = 0; i < posted.length; i++){
+            if(posted[i].name == req.body.name && posted[i].pass == req.body.password){
+              res.cookie('loggedin', '1', { path: '/', expires: new Date(Date.now() + 900000), httpOnly: true });
+              res.cookie('user', req.body.name, { path: '/', expires: new Date(Date.now() + 900000), httpOnly: true });
+              res.send('You logged in!');  
+            } else {
+              res.send('sorry, try again');
+            }
+          }
+          db.close();
+        });
+      });
+    });
+  });
+});
+
+app.get('/logout', function(req,res){
+  res.clearCookie('loggedin');
+  res.send('logged out');
 });
 
 app.listen(3000);
+
 console.log('running at localhost:3000');
