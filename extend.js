c374ffaa4afd272b5c90b49d0a37f6300e9504d7
Brad Davis <brad.bdavis1@gmail.com>
2011-05-21 02:31:45 -0400

    clean up repo

--- extend.js
+++
@@ -1,1 +0,0 @@
-exports.extend = 'extended';
--- murray/setup.js
+++
@@ -1,8 +0,0 @@
-
-if(process.argv[2] != undefined){
-  process.argv.forEach(function(val,index,array){
-    console.log(index + ':' + val);
-  });
-} else {
-  console.log('no arguments');
-}
